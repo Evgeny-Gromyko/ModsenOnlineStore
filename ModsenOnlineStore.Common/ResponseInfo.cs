534b7230766abebf80341c9d879b4e669e486fb6
mariaraniuk <mashenka0110@gmail.com>
2023-06-20 13:21:53 +0300

    Refactoring

--- ModsenOnlineStore.Common/ResponseInfo.cs
+++ ModsenOnlineStore.Common/ResponseInfo.cs
@@ -3,6 +3,7 @@ namespace ModsenOnlineStore.Common;
 public class ResponseInfo
 {
     public bool Success { get; set; }
+    
     public string Message { get; set; }
 
     public ResponseInfo(bool success, string message)
